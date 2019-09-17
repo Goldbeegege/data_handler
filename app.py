10539b24be5d15a61e648f450c1703deeb6d348f
crush <jinfengxuancheng@163.com>
2019-09-17 20:43:20 +0800

    完成

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 
 
 import sys
-from PyQt5.QtWidgets import QApplication,QFileDialog,QMainWindow,QInputDialog,QMessageBox
+from PyQt5.QtWidgets import QApplication,QFileDialog,QMainWindow,QInputDialog,QMessageBox,QPushButton
 from start import Ui_MainWindow
 import datetime
 from dataHandler import data_handler
@@ -14,37 +14,129 @@ class App(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(App, self).__init__(*args, **kwargs)
         self.status = {} #标记当前是什么模式
+        self.last_mode = None #上一次处理文件的模式
         self.file_name = None #要处理的文件的文件名，存为实例变量方便后面获取
-        self.function_button = [] #存放有哪些功能
+        self.mode_button = [] #存放有哪些模式
+        self.function_button = [] #存放功能按钮
+        self.activate_button = None #表示功能按钮具体哪一个被激活
+
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.origin_button = [self.ui.pushButton, self.ui.pushButton_2, self.ui.pushButton_4]
+        #存放上个最基本的按钮，方便以后进行扩展
+        #激活的按钮只能是这三个，并且根据该按钮在self.function_button中的索引进行层级判断；
+        #若索引值为2，那么索引小于等于2的按钮全部被激活，大于2的全部未被激活，为了方便管理功能按钮是否能都点击
 
         for i in range(self.ui.horizontalLayout.count()):
             wg = self.ui.horizontalLayout.itemAt(i).widget()
-            if wg is not None:
-                self.function_button.append(wg)
+            if isinstance(wg,QPushButton):
+                self.mode_button.append(wg)
                 wg.clicked.connect(self.dispatch)
+                #获取所有的模式按钮并且分发槽函数
 
-        self.ui.pushButton.clicked.connect(self.open_file)
-        self.ui.pushButton_2.clicked.connect(self.start_handler)
-        self.ui.pushButton_4.clicked.connect(self.export)
+        for j in range(self.ui.verticalLayout_2.count()):
+            func_tool = self.ui.verticalLayout_2.itemAt(j).widget()
+            if isinstance(func_tool,QPushButton):
+                func_tool.clicked.connect(getattr(self,func_tool.objectName()))
+                self.function_button.append(func_tool)
+                #获取所有的功能按钮并分发槽函数
 
-    def callback(self,color,msg,func_name=None):
-        self.log_msg(color,msg)
-        if func_name is not None:
-            self.ui.pushButton_4.setEnabled(True)
-            self.ui.pushButton_4.setObjectName(func_name)
+    def dispatch(self):
+        """
+        统一接收有模式按钮的信号，并根据反射指定处理函数
+        :return:
+        """
+        sender = self.sender()
+        if self.last_mode != sender.objectName():
+            #判断这次点击的模式是不是上一次处理的，若已经处理过，则可以直接导出，提高用户体验
+            self.activate_button = self.origin_button[0]
+            self.activateButton()
 
+        else:
+            self.activate_button = self.origin_button[-1]
+            self.activateButton()
+
+        for button in self.mode_button:
+            if button==sender:
+                button.setStyleSheet("color:red") #模式按钮选中的样式
+                button.setEnabled(False) #选中或不可在点击
+                if hasattr(self,sender.objectName()):
+                    self.log_msg("red","模式：{}".format(sender.text())) #将模式信息写入文本框中
+                    self.status = sender
+                else:
+                    print("method `{}` must be implement".format(sender.objectName()))
+            else:
+                button.setStyleSheet("color:black")
+                button.setEnabled(True)
+
+    def activateButton(self):
+        """
+        根据索引判断激活按钮的层级关系
+        :return:
+        """
+        index = self.function_button.index(self.activate_button)
+        for i,button in enumerate(self.function_button):
+            if i <= index:
+                button.setEnabled(True)
+            else:
+                button.setEnabled(False)
+
+    def open_file(self):
+        """
+        功能按钮导入的槽函数
+        :return:
+        """
+        file_name,_ = QFileDialog.getOpenFileName(self,"打开文件",".","文件类型(*.csv)")
+        if file_name:
+            self.log_msg("blue","已导入：{}".format(file_name))
+            self.activate_button = self.origin_button[1]
+            self.activateButton()
+            self.file_name = file_name
+        else:
+            self.cancel()
+
+    def start_handler(self):
+        """
+        开始的槽函数
+        :return:
+        """
+        self.log_msg("purple","开始处理：{}<br>".format(self.file_name))
+        getattr(self,self.status.objectName())()
+        data_handler.__init__()
+
+    def callback(self,**kwargs):
+        """
+        回调函数，当某一个功能完成之后毁掉该函数，主要是在文本框中书写内容
+        :param kwargs:
+        :return:
+        """
+        self.log_msg(kwargs.get("color","purple"),kwargs.get("msg"))
+
+        if not kwargs.get("error"):
+            self.activate_button = self.origin_button[-1]
+            self.last_mode = self.status.objectName()
+        else:
+            self.activate_button = self.origin_button[0]
+        self.activateButton()
 
     def export(self):
+        """
+        导出按钮的槽函数
+        :return:
+        """
         path,types = QFileDialog.getSaveFileName(self,"保存文件",self.file_name,"文件类型(*.csv)")
         if path and types:
             data_handler.export(path,self.callback)
         else:
             self.cancel()
 
-
     def log_msg(self,color="purple",msg=None):
+        """
+        向文本框中写出当前处理的状态
+        :param color: 颜色
+        :param msg: 处理信息
+        :return:
+        """
         if msg is None:
             msg = "<font style='color:{}'>{} &nbsp;&nbsp;处理中，请稍后......</font><br>".format(color,datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
         else:
@@ -52,66 +144,54 @@ class App(QMainWindow):
         self.ui.textEdit.append(msg)
 
     def cancel(self):
+        #点击对话框关闭按钮的动作
         self.log_msg("cyan","已取消")
 
-    def dispatch(self):
-        sender = self.sender()
-        if self.ui.pushButton_4.objectName() != sender.objectName():
-            self.ui.pushButton_2.setEnabled(False)
-            self.ui.pushButton_4.setEnabled(False)
-        else:
-            self.ui.pushButton_2.setEnabled(True)
-            self.ui.pushButton_4.setEnabled(True)
+    def muti_task(self,func,*args):
+        """
+        开启多线程
+        :param func:指定多线程的函数
+        :param args: 函数的参数
+        :return:
+        """
+        t1 = threading.Thread(target=self.log_msg)
+        t1.start()
+        t2 = threading.Thread(target=func, args=args)
+        t2.setDaemon(True)
+        t2.start()
 
-        for button in self.function_button:
-            if button==sender:
-                button.setStyleSheet("color:red")
-                button.setEnabled(False)
-                if hasattr(self,sender.objectName()):
-                    self.log_msg("red","模式：{}".format(sender.text()))
-                    self.status = sender
-                    self.ui.pushButton.setEnabled(True)
-                else:
-                    print("method `{}` must be implement".format(sender.objectName()))
-            else:
-                button.setStyleSheet("color:black")
-                button.setEnabled(True)
-
-    def start_handler(self):
-        self.log_msg("purple","开始处理：{}<br>".format(self.file_name))
-        getattr(self,self.status.objectName())()
-
-
-    def open_file(self):
-        file_name,_ = QFileDialog.getOpenFileName(self,"打卡文件",".","文件类型(*.csv)")
-        if file_name:
-            self.log_msg("blue","已导入：{}".format(file_name))
-            self.ui.pushButton_2.setEnabled(True)
-            self.file_name = file_name
-        else:
-            self.cancel()
-
-
+    #下面是具体的功能函数
     def take_part(self):
         frequence,_ = QInputDialog.getInt(self,"请确认频率","频率默认为",6)
         if frequence and _:
             ele,ok = QInputDialog.getItem(self,"请选择具体元素","元素列表为",[str(i+1) for i in range(frequence)])
             if ele and ok:
-                t1 = threading.Thread(target=self.log_msg)
-                t1.start()
-                t2 = threading.Thread(target=data_handler.take_part,args=(int(frequence),self.file_name,int(ele),self.callback))
-                t2.setDaemon(True)
-                t2.start()
+                self.muti_task(data_handler.take_part,*(int(frequence),self.file_name,int(ele),self.callback))
         else:
             self.cancel()
 
-
     def mean_value(self):
-        t1 = threading.Thread(target=self.log_msg)
-        t1.start()
-        t2 = threading.Thread(target=data_handler.mean_value,args=(self.file_name,self.callback))
-        t2.setDaemon(True)
-        t2.start()
+        self.muti_task(data_handler.mean_valu,*(self.file_name,self.callback))
+
+    def rate_value(self):
+        rate,ok = QInputDialog.getText(self,"请选择比例","默认比例为",text="1/2")
+        if rate and ok:
+            self.muti_task(data_handler.rate_value,*(self.file_name,rate,self.callback))
+        else:
+            self.cancel()
+
+    def increasing_value(self):
+        count, ok = QInputDialog.getInt(self, "请选择风速连续不减的个数", "风速连续不减的个数为", value=5)
+        if count and ok:
+            speed, _ = QInputDialog.getText(self, "限制风速", "最大风速不小于", text="5.0")
+
+            if speed and _:
+                self.muti_task(data_handler.increasing_value,*(self.file_name, count,speed,self.callback))
+            else:
+                self.cancel()
+        else:
+            self.cancel()
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- dataHandler.py
+++ dataHandler.py
@@ -5,12 +5,17 @@
 import pandas as pd
 import numpy as np
 
-class DataHander(object):
+
+class BaseHandler(object):
+    def __int__(self):
+        self.data = None
+
+class DataHander(BaseHandler):
     """
     数据处理
     """
     def __init__(self):
-        self.data = None
+        self.ret = {"msg":"","color":"green","error":False}
 
     def take_part(self,frequence,file_name,ele,cb=None):
         """
@@ -25,56 +30,146 @@ class DataHander(object):
         :return:
         """
         df = pd.read_csv(file_name, low_memory=False)
-        df["datetime"] = df["date"] + df["time"]
-        grouped = df.groupby(by=["datetime"])
+        try:
+            df["datetime"] = df["date"] + df["time"]
+            grouped = df.groupby(by=["datetime"])
 
-        data_list = [None for i in range(frequence)]
-        for second, item in grouped:
-            for row in range(frequence):
-                try:
-                    value = np.vstack((data_list[row], item.iloc[row].values))
-                    data_list[row] = value
-                except ValueError:
-                    data_list[row] = item.iloc[row]
-                except IndexError:
-                    break
+            data_list = [None for i in range(frequence)]
+            for second, item in grouped:
+                for row in range(frequence):
+                    try:
+                        value = np.vstack((data_list[row], item.iloc[row].values))
+                        data_list[row] = value
+                    except ValueError:
+                        data_list[row] = item.iloc[row]
+                    except IndexError:
+                        break
 
-        for i, test_data in enumerate(data_list):
-            if i == ele-1:
-                self.data = pd.DataFrame(test_data, columns=df.columns)
+            for i, test_data in enumerate(data_list):
+                if i == ele-1:
+                    self.data = pd.DataFrame(test_data, columns=df.columns)
 
-        msg = "拆分完成：{}".format(file_name)
+            msg = "拆分完成 {}".format(file_name)
+            self.ret["msg"] = msg
+
+        except Exception as e:
+            self.ret["msg"] = str(e)
+            self.ret["color"] = "red"
+            self.ret["error"] = True
+
         if cb is not None and callable(cb):
-            cb("green",msg,"take_part")
+            cb(**self.ret)
 
     def mean_value(self,path,cb=None):
         df = pd.read_csv(path)
-        grouped = df.groupby(by=[df["wind"]])
-        columns = None
-        values = None
-        for wind, item in grouped:
-            data = item.mean(axis=0)
-            if columns is None:
-                print(data)
-                columns = data.index
-                values = data.values
-            else:
-                try:
-                    values = np.vstack((values, data.values))
-                except Exception as e:
-                    pass
-        self.data = pd.DataFrame(values,columns=columns)
-        if cb is not None and callable(cb):
-            msg = "处理完成"
-            cb("green",msg,"mean_value")
+        try:
+            grouped = df.groupby(by=[df["wind"]])
+            columns = None
+            values = None
+            for wind, item in grouped:
+                data = item.mean(axis=0)
+                if columns is None:
+                    print(data)
+                    columns = data.index
+                    values = data.values
+                else:
+                    try:
+                        values = np.vstack((values, data.values))
+                    except Exception as e:
+                        pass
+            self.data = pd.DataFrame(values,columns=columns)
+            msg = "处理完成 {}".format(path)
+            self.ret[msg] = msg
 
+
+        except Exception as e:
+            self.ret["msg"] = str(e)
+            self.ret["color"] = "red"
+            self.ret["error"] = True
+
+        if cb is not None and callable(cb):
+            cb(**self.ret)
+
+    def rate_value(self,file_name,rate,cb=None):
+        df = pd.read_csv(file_name)
+        error = False
+        try:
+            grouped = df.groupby(by=["wind"])
+            column = None
+            data = None
+            m,n = rate.split("/")
+            for wind, item in grouped:
+                length = item.shape[0]
+
+                if length*int(m) < int(n):
+                    continue
+
+                interval, b = divmod(length*int(m), int(n))
+                item = item.sort_values(by=["ang_strain"], ascending=False)
+                value = item.iloc[0:interval]
+                if column is None:
+                    column = value.columns
+                    data = value.values
+                else:
+                    data = np.vstack((data, value.values))
+            self.data = pd.DataFrame(data, columns=column)
+            msg = "处理完成 {}".format(file_name)
+            self.ret[msg] = msg
+
+        except Exception as e:
+            self.ret["msg"] = str(e)
+            self.ret["color"] = "red"
+            self.ret["error"] = True
+
+        if cb is not None and callable(cb):
+            cb(**self.ret)
+
+    def increasing_value(self, file_name, count, speed, cb):
+        df = pd.read_csv(file_name)
+        data_list = []
+        temp_list = []
+        try:
+            for i in range(df.shape[0]):
+                if not temp_list:
+                    temp_list.append(pd.DataFrame(df.iloc[i].to_dict(), index=[0]))
+                else:
+                    if df.iloc[i]["wind"] > temp_list[-1]["wind"][0]:
+                        temp_list.append(pd.DataFrame(df.iloc[i].to_dict(), index=[0]))
+                    else:
+                        if len(temp_list) >= int(count) and temp_list[-1]["wind"][0] > float(speed):
+                            data = temp_list[0].append(temp_list[1:], ignore_index=True)
+                            interval = pd.DataFrame(
+                                np.array([" " for i in range(data.shape[-1])]).reshape(1, data.shape[-1]),
+                                columns=data.columns.to_list())
+                            data = data.append(interval, ignore_index=True)
+                            data_list.append(data)
+                        temp_list.clear()
+            msg = "处理完成 {}".format(file_name)
+            self.ret["msg"] = msg
+
+            if len(data_list) == 0:
+                msg = "未找到符合条件的数据"
+                self.ret["color"] = "red"
+                self.ret["msg"] = msg
+
+            elif len(data_list) == 1:
+                self.data = data_list[0]
+            else:
+                self.data = data_list[0].append(data_list[1:], ignore_index=True)
+        except Exception as e:
+            self.ret["msg"] = str(e)
+            self.ret["color"] = "red"
+            self.ret["error"] = True
+
+        if cb is not None and callable(cb):
+            cb(**self.ret)
 
     def export(self,path,cb=None):
         file_name = path.rsplit("/",1)[1]
         self.data.to_csv(file_name)
-        msg = "已导出完成".format(file_name)
+        msg = "已导出 {}".format(file_name)
+        self.ret["msg"] = msg
         if cb is not None and callable(cb):
-            cb("green",msg)
-
+            cb(**self.ret)
 
 data_handler = DataHander()
